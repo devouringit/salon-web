37389f607d9d002606d9a8c0959c2a2ab3847847
Danny <dnyaneshwar@devourin.com>
2021-10-03 16:27:58 +0530

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+const withPWA = require('next-pwa')
 
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'app/styles')],
   },
@@ -9,5 +13,5 @@ module.exports = {
     //true == Dangerously allow production builds to successfully complete even if  your project has type errors.
     ignoreBuildErrors: false,
   },
-    reactStrictMode: true,
-};
+  reactStrictMode: true,
+})
